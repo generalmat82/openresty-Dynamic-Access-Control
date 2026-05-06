b8eb1e275a639664c4e21f30b2d6f4eb33fab52b
generalmat82 <generalmat81@gmail.com>
2026-05-06 00:23:51 -0400

    removed the need for init

--- access_functions/blocking.lua
+++ access_functions/blocking.lua
@@ -1,21 +1,21 @@
 local BLOCKING = {}
 
-function BLOCKING.blockCheck(blockKey,clientIP,SECRETS,DB)
+function BLOCKING.blockCheck(blockKey,clientIP,SECRETS,DB,CACHES)
     -- This function verifies if the IP is already blocked.
-    if BL_CACHE:get(blockKey) == "true" then
+    if CACHES.BL:get(blockKey) == "true" then
         -- the IP is in the blacklist cache.
         BLOCKING.retryAttempt(blockKey)
         ngx.exit(ngx.HTTP_FORBIDDEN)
     end
     if DB:get(blockKey) == "true" then
-        BL_CACHE:set(blockKey, true, SECRETS.cache.bl_ttl)
+        CACHES.BL:set(blockKey, true, SECRETS.cache.bl_ttl)
         BLOCKING.retryAttempt(blockKey)
         ngx.exit(ngx.HTTP_FORBIDDEN)
     end
     local SUBNETS = require("init_functions.init_subnet")
     if SUBNETS.BLOCKED:match(clientIP) == true then
         DB:set(blockKey,true)
-        BL_CACHE:set(blockKey,true,SECRETS.cache.bl_ttl)
+        CACHES.BL:set(blockKey,true,SECRETS.cache.bl_ttl)
         return true end
 end
 
@@ -36,14 +36,14 @@ function BLOCKING.thresholdCheck(blockKey,countKey,SECRETS,DB)
     end
 end
 
-function BLOCKING.geo_check(clientIP,blockKey,SECRETS,DB,GENERAL)
+function BLOCKING.geo_check(clientIP,blockKey,SECRETS,DB,GENERAL,CACHES)
     -- block based on IP geolocation.
     local geo = require("init_functions.init_geo")
     local country = geo.lookup(clientIP, nil, 'country')
     if GENERAL.has_value(SECRETS.geoip.blocked_countries, country) then
         DB:set(blockKey, true)
         DB:expire(blockKey, SECRETS.block.block_time)
-        BL_CACHE:set(blockKey,true,SECRETS.cache.bl_ttl)
+        CACHES.BL:set(blockKey,true,SECRETS.cache.bl_ttl)
         ngx.exit(ngx.HTTP_FORBIDDEN)
     end
 end
--- init.lua
+++
@@ -1,6 +0,0 @@
-local SECRETS = require "secrets"
-GEO = require 'resty.maxminddb'
-
--- Cache Initialization
-BL_CACHE = ngx.shared.ip_blacklist_cache
-WT_CACHE = ngx.shared.ip_whitelist_cache
