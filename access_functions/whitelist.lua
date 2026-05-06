1a95652780f80c130d3589d6e3c8338b11e82a0b
generalmat82 <110256073+generalmat82@users.noreply.github.com>
2026-05-05 23:28:00 -0400

    Implementation of new ip in subnet library. (#8)

--- access_functions/whitelist.lua
+++ access_functions/whitelist.lua
@@ -8,7 +8,7 @@ function WHITELIST.whitelistCheck(whitelistKey,clientIP,SECRETS,DB)
     if DB:get(whitelistKey) == "true" then
         WT_CACHE:set(whitelistKey,true,SECRETS.cache.wt_ttl)
         return true end
-    if ALLOWED_IPS:isInSubnets(clientIP) == true then
+    if ALLOWED_IPS:match(clientIP) == true then
         DB:set(whitelistKey,true)
         WT_CACHE:set(whitelistKey,true,SECRETS.cache.wt_ttl)
         return true end
