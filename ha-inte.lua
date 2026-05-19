211e580ba9dd7b584d75896639c152a4d0987ea3
generalmat82 <generalmat81@gmail.com>
2026-05-19 01:10:56 +0000

    could not use URI args so had to swap how it would work.

--- ha-inte.lua
+++ ha-inte.lua
@@ -1,13 +1,21 @@
+-- -File Import
 local SECRETS = require "secrets"
 local REDIS_CON = require "general_functions.redis_con"
 local DB = REDIS_CON.get_redis_connection(SECRETS)
-if ngx.var.uri == "/add" then
-    local ipAddr = ngx.var.arg_ip
-    DB:set("whitelist:ip:"..ipAddr,true)
-    ngx.print("Added IP:"..ipAddr)
+local cjson = require "cjson"
+
+-- -Obtaining body
+ngx.req.read_body()
+local params = cjson.decode(ngx.req.get_body_data())
+
+-- -Actions - Add
+if params["action"] == "add" then
+    DB:set("whitelist:ip:"..params["ip"],true)
+    ngx.print("Added IP:"..params["ip"])
 end
-if ngx.var.uri == "/remove" then
-    local ipAddr = ngx.var.arg_ip
-    DB:del("whitelist:ip:"..ipAddr)
-    ngx.print("Removed IP: "..ipAddr)
+
+-- -Actions - Remove
+if params["action"] == "remove" then
+    DB:del("whitelist:ip:"..params["ip"])
+    ngx.print("Removed IP: "..params["ip"])
 end
