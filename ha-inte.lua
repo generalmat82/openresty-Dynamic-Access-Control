0dacea7f2a7e9ddfdfe3b945f9d5321920daa902
generalmat82 <generalmat81@gmail.com>
2026-05-28 02:22:13 +0000

    Updated ha-inte to allow for comments

--- ha-inte.lua
+++ ha-inte.lua
@@ -7,15 +7,24 @@ local cjson = require "cjson"
 -- -Obtaining body
 ngx.req.read_body()
 local params = cjson.decode(ngx.req.get_body_data())
+local whitelistKey = "whitelist:ip:"..params["ip"]
+
+-- -Verif of HA entries
+local wtVal = DB:get(whitelistKey) 
+
+if not wtVal == "true#ha" or type(wtVal) == "nil" then
+    ngx.print("entry not changed. Not entered via HA")
+    ngx.exit()
+end
 
 -- -Actions - Add
 if params["action"] == "add" then
-    DB:set("whitelist:ip:"..params["ip"],true)
+    DB:set(whitelistKey,"true#ha")
     ngx.print("Added IP:"..params["ip"])
 end
 
 -- -Actions - Remove
 if params["action"] == "remove" then
-    DB:del("whitelist:ip:"..params["ip"])
+    DB:del(whitelistKey)
     ngx.print("Removed IP: "..params["ip"])
 end
